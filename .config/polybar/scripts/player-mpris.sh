484bfc9520128aeac4cde9af9cb09a9c66debdbc
Daniel Jimenez Jerez <djimenezjerez@gmail.com>
2019-07-19 10:36:06 -0400

    First commit

---
+++ .config/polybar/scripts/player-mpris.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+player_status=$(playerctl status 2> /dev/null)
+
+if [ "$player_status" = "Playing" ]; then
+    echo " $(playerctl metadata artist | sed 's/^\(.\{42\}\).*/\1/g') - $(playerctl metadata title | sed 's/^\(.\{42\}\).*/\1/g')"
+elif [ "$player_status" = "Paused" ]; then
+    echo " $(playerctl metadata artist | sed 's/^\(.\{42\}\).*/\1/g') - $(playerctl metadata title | sed 's/^\(.\{42\}\).*/\1/g')"
+else
+    echo ""
+fi
