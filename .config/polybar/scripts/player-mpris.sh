852015cd3e372c5fef657c4e7cd76f5df5faeb2a
Daniel Jimenez Jerez <djimenezjerez@gmail.com>
2019-07-23 20:07:12 -0400

    Removed artist on mpris playerctl script

--- .config/polybar/scripts/player-mpris.sh
+++ .config/polybar/scripts/player-mpris.sh
@@ -3,9 +3,11 @@
 player_status=$(playerctl status 2> /dev/null)
 
 if [ "$player_status" = "Playing" ]; then
-    echo " $(playerctl metadata artist | sed 's/^\(.\{42\}\).*/\1/g') - $(playerctl metadata title | sed 's/^\(.\{42\}\).*/\1/g')"
+#    echo " $(playerctl metadata artist | sed 's/^\(.\{42\}\).*/\1/g') - $(playerctl metadata title | sed 's/^\(.\{42\}\).*/\1/g')"
+    echo " $(playerctl metadata title | sed 's/^\(.\{42\}\).*/\1/g')"
 elif [ "$player_status" = "Paused" ]; then
-    echo " $(playerctl metadata artist | sed 's/^\(.\{42\}\).*/\1/g') - $(playerctl metadata title | sed 's/^\(.\{42\}\).*/\1/g')"
+#    echo " $(playerctl metadata artist | sed 's/^\(.\{42\}\).*/\1/g') - $(playerctl metadata title | sed 's/^\(.\{42\}\).*/\1/g')"
+    echo " $(playerctl metadata title | sed 's/^\(.\{42\}\).*/\1/g')"
 else
     echo ""
 fi
